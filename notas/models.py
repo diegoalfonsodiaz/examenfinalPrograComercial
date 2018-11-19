a37e2b715ff78c509ca8c89061dc50a00fc6c4c6
diego <diegoalfonsodiaz@gmail.com>
2018-11-19 11:44:23 -0600

    nuevo modelo

--- notas/models.py
+++ notas/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Materia(models.Model):
     nombre  =   models.CharField(max_length=250)
     catedratico = models.CharField(max_length=250)
@@ -38,3 +39,45 @@ class MateriaAdmin(admin.ModelAdmin):
 
 class PensumAdmin (admin.ModelAdmin):
     inlines = (AsignacionInLine,)
+
+
+
+
+class Carrera(models.Model):
+    nombre  =   models.CharField(max_length=250)
+    descripccion = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Factultad(models.Model):
+    nombre    = models.CharField(max_length=250)
+    ciclo      = models.IntegerField() 
+    carrras   = models.ManyToManyField(Materia, through='AsignacionCarrera')
+    def __str__(self):
+        return self.nombre
+
+class AsignacionCarrera(models.Model):
+    materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
+    pensum = models.ForeignKey(Pensum, on_delete=models.CASCADE)
+    
+
+class AsignacionCarreraInLine(admin.TabularInline):
+    model = Asignacion
+    extra = 1
+
+
+class FacultadAdmin(admin.ModelAdmin):
+    inlines = (AsignacionCarreraInLine,)
+
+
+class CarreraAdmin (admin.ModelAdmin):
+    inlines = (AsignacionCarreraInLine,)
+
+class Catedratico(models.Model):
+    nombre    = models.CharField(max_length=250)
+    titulo    = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
